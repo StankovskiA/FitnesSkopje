9c7b93ab2dc483d7117471d6108e64ee73550816
Petar <zafirovskipetar@gmail.com>
2021-11-09 10:30:00 +0100

    ASP .NET Web app

---
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnesSkopjeWebApp.Startup))]
+namespace FitnesSkopjeWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
