854924a77622c3caac13f70232c234ac728c40f5
SaraMisajlovska <smisajlovska@gmail.com>
2021-12-01 21:03:21 +0100

    Controllers and Views Created, Home page and Nav bar updated

---
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/FavouritesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FitnesSkopjeWebApp.Models;
+
+namespace FitnesSkopjeWebApp.Controllers
+{
+    public class FavouritesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Favourites
+        public ActionResult Index()
+        {
+            return View(db.Favourites.ToList());
+        }
+
+        // GET: Favourites/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Favourite favourite = db.Favourites.Find(id);
+            if (favourite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(favourite);
+        }
+
+        // GET: Favourites/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Favourites/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,userId,gymId")] Favourite favourite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Favourites.Add(favourite);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(favourite);
+        }
+
+        // GET: Favourites/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Favourite favourite = db.Favourites.Find(id);
+            if (favourite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(favourite);
+        }
+
+        // POST: Favourites/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,userId,gymId")] Favourite favourite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(favourite).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(favourite);
+        }
+
+        // GET: Favourites/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Favourite favourite = db.Favourites.Find(id);
+            if (favourite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(favourite);
+        }
+
+        // POST: Favourites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Favourite favourite = db.Favourites.Find(id);
+            db.Favourites.Remove(favourite);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/GymsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FitnesSkopjeWebApp.Models;
+
+namespace FitnesSkopjeWebApp.Controllers
+{
+    public class GymsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Gyms
+        public ActionResult Index()
+        {
+            return View(db.Gyms.ToList());
+        }
+
+        // GET: Gyms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gym gym = db.Gyms.Find(id);
+            if (gym == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gym);
+        }
+
+        // GET: Gyms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gyms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,Number,WorkingTime,Areas")] Gym gym)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gyms.Add(gym);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gym);
+        }
+
+        // GET: Gyms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gym gym = db.Gyms.Find(id);
+            if (gym == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gym);
+        }
+
+        // POST: Gyms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,Number,WorkingTime,Areas")] Gym gym)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gym).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gym);
+        }
+
+        // GET: Gyms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gym gym = db.Gyms.Find(id);
+            if (gym == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gym);
+        }
+
+        // POST: Gyms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gym gym = db.Gyms.Find(id);
+            db.Gyms.Remove(gym);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
