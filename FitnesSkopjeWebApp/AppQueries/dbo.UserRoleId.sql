0bafcff48b7d9ced60340ab96834893af672569e
Petar <zafirovskipetar@gmail.com>
2021-12-01 13:39:00 +0100

    update test

--- FitnesSkopjeWebApp/AppQueries/dbo.UserRoleId.sql
+++ FitnesSkopjeWebApp/AppQueries/dbo.UserRoleId.sql
@@ -1,5 +1,8 @@
 ﻿alter table Users
 add roleId nvarchar(128) not null;
+use [aspnet-FitnesSkopjeWebApp-20211109102850]
+alter table Users
+drop constraint FK_UserRoleId;
 
 alter table Users
 add constraint FK_UserRoleId foreign key(roleId) references AspNetRoles(Id);
