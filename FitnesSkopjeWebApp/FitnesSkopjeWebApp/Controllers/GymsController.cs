959022069ba612a9ce6e1c9a7e385738d0273c35
SaraMisajlovska <smisajlovska@gmail.com>
2021-12-06 20:36:09 +0100

    Buttons Updated

--- FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/GymsController.cs
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/GymsController.cs
@@ -10,7 +10,7 @@ using FitnesSkopjeWebApp.Models;
 
 namespace FitnesSkopjeWebApp.Controllers
 {
-    [Authorize(Roles ="Admin,User")]
+    [Authorize(Roles = "Admin,User")]
     public class GymsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
