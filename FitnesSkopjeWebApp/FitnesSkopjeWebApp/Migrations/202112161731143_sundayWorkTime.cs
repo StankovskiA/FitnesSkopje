9ae17a4b4515db2cf57eb99f08b7f40cf21f28b6
Petar <zafirovskipetar@gmail.com>
2021-12-16 18:41:18 +0100

    Database update & modification

---
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Migrations/202112161731143_sundayWorkTime.cs
@@ -0,0 +1,16 @@
+namespace FitnesSkopjeWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sundayWorkTime : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
