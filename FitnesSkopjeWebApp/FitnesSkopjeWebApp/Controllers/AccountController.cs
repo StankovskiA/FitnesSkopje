a84771e3f0ff83f39e4cee0968ae76900a5f196f
Petar <zafirovskipetar@gmail.com>
2021-12-27 16:18:47 +0100

    Added registration confirmation

--- FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/AccountController.cs
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/AccountController.cs
@@ -190,13 +190,18 @@ namespace FitnesSkopjeWebApp.Controllers
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("LogInFitnessSkopje");
                 }
                 AddErrors(result);
             }
 
             // If we got this far, something failed, redisplay form
             return View(model);
+        }
+
+        public ActionResult LogInFitnessSkopje()
+        {
+            return View();
         }
 
         //
