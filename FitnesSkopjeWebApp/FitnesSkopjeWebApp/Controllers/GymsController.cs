c1ac09de0778a4b812c1492fa2ea181069d3d4b0
Petar <zafirovskipetar@gmail.com>
2022-01-30 16:39:10 +0100

    Update GymsController.cs

--- FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/GymsController.cs
+++ FitnesSkopjeWebApp/FitnesSkopjeWebApp/Controllers/GymsController.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using FitnesSkopjeWebApp.Helper;
 using FitnesSkopjeWebApp.Models;
 using FitnesSkopjeWebApp.ViewModels;
 using PagedList;
